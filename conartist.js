972c7d999823e0155ffd2278b3e204c64f68bd12
Trey Shugart <treshugart@gmail.com>
2017-11-15 09:07:14 +1100

    Fix babel ignore and rollup.

--- conartist.js
+++ conartist.js
@@ -1,8 +1,10 @@
 const { config, preset } = require('conartist');
-
-module.exports = config(
-  preset.base(),
-  preset.babel(),
-  preset.jest(),
-  preset.rollup({ name: 'css-in-js' })
-);
+const rollup = preset.rollup();
+module.exports = config(preset.base(), preset.babel(), preset.jest(), {
+  ...rollup,
+  ...{
+    ['rollup.config.js']() {
+      return { ...{ name: 'yocss' }, ...rollup['rollup.config.js']() };
+    }
+  }
+});
